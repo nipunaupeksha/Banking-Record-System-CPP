e6433d1bd7abc3082c9753476bb65b6d853193fe
nipunaupeksha <nipuna.upeksha@gmail.com>
2019-07-05 09:02:06 +0530

    added some snippets

--- Banking Record System/main.cpp
+++ Banking Record System/main.cpp
@@ -11,7 +11,33 @@ using std::ios;
 using namespace std;
 
 class account_query
-{};
+{
+    private:
+        char account_number[20];
+        char firstName[10];
+        char lastName[10];
+        float total_Balance;
+    public:
+        void read_data();
+        void show_data();
+        void write_rec();
+        void read_rec();
+        void search_rec();
+        void edit_rec();
+        void delete_rec();
+};
+void account_query::read_data()
+{
+    cout<<"\nEnter Account Number: ";
+    cin>>account_number;
+    cout<<"Enter First Name: ";
+    cin>>firstName;
+    cout<<"Enter Last Name: ";
+    cin>>lastName;
+    cout>>"Enter Balance: ";
+    cin>>total_Balance;
+    cout<<endl;
+}
 
 int main()
 {
