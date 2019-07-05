50b5ef64696d57d3dbbee110105573dd51282828
nipunaupeksha <nipuna.upeksha@gmail.com>
2019-07-05 08:25:23 +0530

    added the source code file with some code snippets

---
+++ Banking Record System/main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <fstream>
+#include <cstdlib>
+using std::cout;
+using std::cin;
+using std::endl;
+using std::fstream;
+using std::ofstream;
+using std::ifstream;
+using std::ios;
+using namespace std;
+
+class account_query
+{};
+
+int main()
+{
+    cout << "Hello world!" << endl;
+    return 0;
+}
